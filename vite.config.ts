a78f66c6075566258cb193cf9aff138e431fb240
Shombhunath Karan <karanshombhu@gmail.com>
2025-06-19 09:42:23 +0530

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         start_url: '/',
         display: 'standalone',
         background_color: '#ffffff',
-        theme_color: '#1976d2',
+        theme_color: '#181c24',
         icons: [
           {
             src: '/navlogo.png',
